8f84f4938656bbc57d83940130700c4e2d9a28cf
alisher <shera.rashiduulu@gmail.com>
2019-12-11 13:02:18 +0600

    installed api_v1 app, and added to settings.py, also added REST_FRAMEWORK settings to settings.py

--- source/main/settings.py
+++ source/main/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'api_v1'
     'webapp',
 ]
 
@@ -120,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION': (
+        'rest_framework.authentication.Token.Authentication',
+    )
+}
