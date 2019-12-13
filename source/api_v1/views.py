9440b6333949f87f6363f6adeda02fc62d16f4bf
alisher <shera.rashiduulu@gmail.com>
2019-12-13 14:48:56 +0600

    Migrated one more time

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import QuoteSerializer
+from webapp.models import Quote, QUOTE_APPROVED
+
+
+class LogoutView(APIView):
+    permission_classes = []
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        if user.is_authenticated:
+            user.auth_token.delete()
+        return Response({'status': 'OK'})
+
+
+class QuoteViewSet(ModelViewSet):
+    queryset = Quote.objects.none()
+    serializer_class = QuoteSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Quote.objects.all()
+        return Quote.objects.filter(status=QUOTE_APPROVED)
+
+    # доступ к изменению и удалению цитат
+    # только для вошедших пользователей
+    # ...
+    # название точки входа для проверки
+    # доступно в: self.action
+
+    @action(methods=['post'], detail=True)
+    def rate_up(self, request, pk=None):
+        quote = self.get_object()
+        quote.rating += 1
+        quote.save()
+        return Response({'id': quote.pk, 'rating': quote.rating})
+
+
